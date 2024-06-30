22392812889ccca43706a6f4a8b38251bc5c07d4
Diegocaceres97 <diegocaceres1997garcia@gmail.com>
2024-06-30 14:16:12 -0500

    chore: first commit

---
+++ src/components/data/menuData.js
@@ -0,0 +1,10 @@
+export const mainMenu = [
+     {link:"#", content:"Home", icon:"lucide:home"},
+     {link:"#about", content:"About", icon:"octicon:person-24"},
+     {link:"#blog", content:"Blog", icon:"mdi:blog"}
+]
+export const menuBlog = [
+     {link:"/", content:"Home", icon:"lucide:home"},
+     {link:"/#about", content:"About", icon:"octicon:person-24"},
+     {link:"/#blog", content:"Blog", icon:"carbon:portfolio"},
+]
