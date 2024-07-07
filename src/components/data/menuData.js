00d1fb6295e50f2539b9d247f3f07825a31a29dd
Diegocaceres97 <diegocaceres1997garcia@gmail.com>
2024-07-07 12:47:07 -0500

    chore: justify text in about me and skills and image

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,6 +1,6 @@
 export const mainMenu = [
-     {link:"#", content:"Home", icon:"lucide:home"},
-     {link:"#about", content:"About", icon:"octicon:person-24"},
+     {link:"#", content:"Inicio", icon:"lucide:home"},
+     {link:"#about", content:"Acerca", icon:"octicon:person-24"},
      {link:"#blog", content:"Blog", icon:"mdi:blog"}
 ]
 export const menuBlog = [
